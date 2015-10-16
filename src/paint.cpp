2719a7175cc4e0a674be0037aec30a3491188484
Kimmo Lindholm <kimmo.lindholm@eke.fi>
2015-10-16 18:23:14 +0300

    replace translations hardcoded path with dynamic

--- src/paint.cpp
+++ src/paint.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
 
     QTranslator translator;
-    translator.load(QLocale::system().name(), "/usr/share/harbour-paint/i18n");
+    translator.load(QLocale::system().name(), SailfishApp::pathTo("i18n").toString(QUrl::RemoveScheme));
     app->installTranslator(&translator);
 
     QScopedPointer<QQuickView> view(SailfishApp::createView());
